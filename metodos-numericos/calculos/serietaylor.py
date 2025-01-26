b1c6174d1e7c7ccd3619abeac17944203eb31f1b
Jeovani Pacheco <regirabanes@gmail.com>
2025-01-25 22:11:49 -0600

    Update serietaylor.py

--- metodos-numericos/calculos/serietaylor.py
+++ metodos-numericos/calculos/serietaylor.py
@@ -4,4 +4,8 @@ def calcular_serie_taylor(funcion_str, expansion, numero_n):
     x = sp.symbols('x')
     funcion = sp.sympify(funcion_str)
     serie_taylor = sp.series(funcion, x, expansion, numero_n)
-    return str(serie_taylor).replace('**', '^').replace('O(', 'O(')
+    
+    serie_latex = sp.latex(serie_taylor)
+    serie_latex = serie_latex.replace('**', '^').replace('O(', 'O(')
+    return serie_latex
+    #return str(serie_latex).replace('**', '^').replace('O(', 'O(')
