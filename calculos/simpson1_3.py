7a574218118a653379a5e87bea74b0bc99c00b37
Juan <jemmanuelbn2@gmail.com>
2025-02-03 21:43:44 -0600

    Correcciones

--- calculos/simpson1_3.py
+++ calculos/simpson1_3.py
@@ -1,18 +1,31 @@
-import numpy as np
 import sympy as sp
+import math
 
 def simpson1_3(funcion, x0, xn, numero_n):
     if(xn <= x0):
         raise ValueError('El límite superior debe ser mayor al límite inferior')
     
-    if(numero_n < 0):
+    if(numero_n <= 0):
         raise ValueError('El número de términos debe ser mayor a 0')
     
     if(numero_n % 2 == 1):
         raise ValueError('El número de términos debe ser un número par')
     
+    context = {
+        'sin': math.sin,
+        'cos': math.cos,
+        'tan': math.tan,
+        'exp': math.exp,
+        'log': math.log,
+        'sqrt': math.sqrt,
+        'pi': math.pi
+    }
+
+    def g(x):
+        return eval(funcion, {"x": x}, context)
+    
     x = sp.symbols('x')
-    function = sp.lambdify(x, funcion, 'numpy')  # Convierte el string en una expresión simbólica
+    ##function = sp.lambdify(x, funcion, 'numpy')  # Convierte el string en una expresión simbólica
 
     h = (xn - x0) / numero_n
 
@@ -20,8 +33,8 @@ def simpson1_3(funcion, x0, xn, numero_n):
     for i in range(0,(numero_n-2),2):
         xi = x0 + i*h
         xi2 = x0 + (i+1)*h
-        sum += 4*function(xi) + 2*function(xi2)
+        sum += 4*g(xi) + 2*g(xi2)
     xh = xn - h
-    sum += 4*function(xh) + function(xn)
+    sum += 4*g(xh) + g(xn)
 
     return (h*sum)/3
